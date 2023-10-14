90f87b2801fffc7544cbdbfcd9a86b4ebaa04a14
Dario Lopez <dlopez@shutterstock.com>
2023-10-14 17:14:14 +0200

    today: styling

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,15 +1,24 @@
 import styles from './Today.module.css';
 
 export default function Today(props){
+    console.log("props", props);
     return (
         <div className={styles.Today}>
-            <div>
-                {props.data.current.condition.text} <img src={props.data.current.condition.icon} alt={props.data.current.condition.text} />
+            <div className={styles.TodayTitle}>
+                {props.data.current.condition.text}
             </div>
-            <div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <img
+                    className={styles.TodayIcon} 
+                    src={props.data.current.condition.icon} alt={props.data.current.condition.text} />
+                <div className={styles.TodayCurrentTemp}>
+                    28º
+                </div>
+            </div>
+            <div className={styles.TodayMinMax}>
                 {props.data.forecast.forecastday[0].day.maxtemp_c}º - {props.data.forecast.forecastday[0].day.mintemp_c}º 
             </div>
-            <div>
+            <div className={styles.TodayFeelsLike}>
                 Feels like {props.data.current.feelslike_c}º
             </div>
         </div>
