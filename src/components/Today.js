dca3481d70bff7c3364412514dff8e263e4a5186
Dario Lopez <dlopez@shutterstock.com>
2023-10-15 18:11:59 +0200

    Today styles improved

--- src/components/Today.js
+++ src/components/Today.js
@@ -3,22 +3,33 @@ import styles from './Today.module.css';
 export default function Today(props){
     return (
         <div className={styles.Today}>
-            <div className={styles.TodayTitle}>
-                {props.data.current.condition.text}
-            </div>
-            <div style={{display: 'flex', justifyContent: 'center'}}>
-                <img
-                    className={styles.TodayIcon} 
-                    src={props.data.current.condition.icon} alt={props.data.current.condition.text} />
-                <div className={styles.TodayCurrentTemp}>
-                    {props.data.current.temp_c}º
+            <div className={styles.TodayGrid}>
+                <div className={styles.TodayIcon}>
+                    <img src={props.data.current.condition.icon} alt={props.data.current.condition.text} />
                 </div>
-            </div>
-            <div className={styles.TodayMinMax}>
-                {Math.round(props.data.forecast.forecastday[0].day.maxtemp_c)}º / {Math.round(props.data.forecast.forecastday[0].day.mintemp_c)}º 
-            </div>
-            <div className={styles.TodayFeelsLike}>
-                Feels like {Math.round(props.data.current.feelslike_c)}º
+                <div className={styles.TodayTemp}>
+                    <div className={styles.TodayCurrentTemp}>
+                        {Math.round(props.data.current.temp_c)}º
+                    </div>
+                    <div className={styles.TodayFeelsLike}>
+                        Feels like {Math.round(props.data.current.feelslike_c)}º
+                    </div>
+                    
+                </div>
+                <div className={styles.TodayInfo}>
+                    <div className={styles.TodayTitle}>
+                        {props.data.current.condition.text}
+                    </div>
+                    <div className={styles.TodayMinMax}>
+                        {Math.round(props.data.forecast.forecastday[0].day.mintemp_c)}º / {Math.round(props.data.forecast.forecastday[0].day.maxtemp_c)}º 
+                    </div>
+                    <div>
+                        Wind: {props.data.current.wind_dir} {props.data.current.wind_kph}km/h
+                    </div>
+                    <div>
+                        Humidity: {props.data.current.humidity}%
+                    </div>
+                </div>
             </div>
         </div>
     );
